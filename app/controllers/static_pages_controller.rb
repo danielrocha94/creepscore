9def437d251c8580635b0a162022c35f9bb96946
danielrocha94 <danielrocha94@msn.com>
2016-10-06 17:29:04 +0200

    Added home page and search basics

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def post_search
+    render nothing: true
+    @summoner = params[:search]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get 'search' => 'static_pages#post_search'
 end
